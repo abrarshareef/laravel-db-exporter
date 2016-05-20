448063966c5134df37c851cd5e56c62be49efe99
Abrar Shareef <abrar.shareef@fticonsulting.com>
2016-05-20 17:01:36 -0400

    changed object property name from signature to "name". Because of this wrong name, the options were not working for the command.

--- src/ViKon/DbExporter/Console/Commands/SeedCommand.php
+++ src/ViKon/DbExporter/Console/Commands/SeedCommand.php
@@ -26,7 +26,7 @@ class SeedCommand extends Command
      */
     public function __construct()
     {
-        $this->signature   = 'vi-kon:db-exporter:seed';
+        $this->name   = 'vi-kon:db-exporter:seed';
         $this->description = 'Create seed files from database tables';
 
         parent::__construct();
